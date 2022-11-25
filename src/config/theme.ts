5a03e62b58db6abd42a33a698307d2d7d64eff8e
usmansbk <usmansbk@gmail.com>
2022-11-25 15:27:52 +0100

    add theming

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,7 +1,10 @@
-const theme = {
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
   colors: {
     primary: "#c35",
     text: "#333",
+    secondary: "#c35",
   },
 };
 
---
+++ src/styled.d.ts
@@ -0,0 +1,11 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      primary: string;
+      secondary: string;
+      text: string;
+    };
+  }
+}
