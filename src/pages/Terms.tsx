a6bcb183b2514d8530ea370eb2edd37f12ccbbe1
usmansbk <usmansbk@gmail.com>
2022-11-26 23:11:36 +0100

    nit

--- src/pages/Terms.tsx
+++ src/pages/Terms.tsx
@@ -2,12 +2,14 @@ import { Box, Stack, Typography } from "@mui/material";
 
 export default function Terms() {
   return (
-    <Stack flexGrow={1}>
+    <Stack flexGrow={1} spacing={4}>
       <Box>
         <Typography variant="h4">Terms of use</Typography>
+        <Typography>Teamzone is currently under development.</Typography>
       </Box>
       <Box>
         <Typography variant="h4">Privacy policy</Typography>
+        <Typography>We current don't tract any information.</Typography>
       </Box>
     </Stack>
   );
