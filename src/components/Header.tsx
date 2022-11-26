d2c50906757866d5258617a67e9ab846852e9bfc
usmansbk <usmansbk@gmail.com>
2022-11-26 20:35:24 +0100

    nit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,15 @@
 import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <Typography variant="h1" color="primary">
+    <Typography
+      variant="h1"
+      color="primary"
+      component={Link}
+      to="/"
+      style={{ textDecoration: "none" }}
+    >
       Teamzone
     </Typography>
   );
