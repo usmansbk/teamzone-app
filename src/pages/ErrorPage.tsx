83620c888c3aee82e7b59a73fc8dbce4302d962d
usmansbk <usmansbk@gmail.com>
2022-11-26 23:28:54 +0100

    nit

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,23 +1,14 @@
-import { Box, Typography, Stack } from "@mui/material";
+import { Typography, Stack } from "@mui/material";
 import { useRouteError } from "react-router-dom";
-import Header from "src/components/Header";
 
 export default function ErrorPage() {
   const error = useRouteError() as Error;
   return (
-    <Box height="100vh" display="flex" flexGrow={1} flexDirection="column">
-      <Header />
-      <Stack
-        flexGrow={1}
-        justifyContent="center"
-        alignItems="center"
-        spacing={4}
-      >
-        <Typography variant="h3">
-          Sorry, an unexpected error has occurred.
-        </Typography>
-        <Typography>{error.message || (error as any).statusText}</Typography>
-      </Stack>
-    </Box>
+    <Stack flexGrow={1} justifyContent="center" alignItems="center" spacing={4}>
+      <Typography variant="h3">
+        Sorry, an unexpected error has occurred.
+      </Typography>
+      <Typography>{error.message || (error as any).statusText}</Typography>
+    </Stack>
   );
 }
