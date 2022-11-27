e4495341bb518f9d438c3e1486d3911f6e913eec
usmansbk <usmansbk@gmail.com>
2022-11-27 16:04:41 +0100

    nit

--- codegen.ts
+++ codegen.ts
@@ -3,6 +3,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
   schema: "http://localhost:4000/graphql",
   documents: ["src/queries/*.ts", "!src/gql/**/*"],
+  ignoreNoDocuments: true,
   generates: {
     "./src/gql/": {
       preset: "client",
