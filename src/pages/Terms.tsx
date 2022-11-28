419cec7e3f5146781ec3b804cd8a402988814378
usmansbk <usmansbk@gmail.com>
2022-11-28 17:13:16 +0100

    update terms

--- src/pages/Terms.tsx
+++ src/pages/Terms.tsx
@@ -5,11 +5,24 @@ export default function Terms() {
     <Stack flexGrow={1} spacing={4}>
       <Box>
         <Typography variant="h4">Terms of use</Typography>
-        <Typography>Teamzone is currently under development.</Typography>
+        <Typography>
+          We do our best to ensure information accuracy but errors may still
+          occur. Therefore the content of this site is provided on "as is"
+          basis. Teamzone and it's owners cannot be held liable for any errors
+          or inaccuracies
+        </Typography>
       </Box>
       <Box>
         <Typography variant="h4">Privacy policy</Typography>
-        <Typography>We currently don't track any information.</Typography>
+        <Typography>
+          If you choose to sign up for an account with us the only data-point we
+          collect and store is your name and email address so that you can join
+          or create your teams. Your information is never sold or disclosed to
+          any 3rd party. For any mass email communication (i.e. newsletter)
+          there will be an opt-out option. To request deletion of your account
+          (and all of the data associated with it), please use the contact us
+          form.
+        </Typography>
       </Box>
     </Stack>
   );
