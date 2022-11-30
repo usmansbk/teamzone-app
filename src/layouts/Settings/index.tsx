0e57536a91c2221ddad1f85ad0e8114bc9e94af3
usmansbk <usmansbk@gmail.com>
2022-11-30 19:32:29 +0100

    add edit page

--- src/layouts/Settings/index.tsx
+++ src/layouts/Settings/index.tsx
@@ -1,11 +1,18 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import Footer from "src/components/Footer";
+import Header from "src/components/Header";
 
 export default function Settings() {
   return (
-    <Box>
-      <Typography>Settings</Typography>
-      <Outlet />
-    </Box>
+    <Container fixed>
+      <Box height="100vh" display="flex" flexGrow={1} flexDirection="column">
+        <Header />
+        <Box display="flex" flexDirection="column" flexGrow={1}>
+          <Outlet />
+        </Box>
+        <Footer />
+      </Box>
+    </Container>
   );
 }
--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -1,9 +1,20 @@
-import { Box, Typography } from "@mui/material";
+import { Box, TextField, Stack, Grid, Typography, Button } from "@mui/material";
 
 export default function EditProfile() {
   return (
     <Box>
-      <Typography>Edit Profile</Typography>
+      <Grid container justifyContent="center">
+        <Grid item xs={12} md={6}>
+          <Stack spacing={2}>
+            <Typography variant="h6">User</Typography>
+            <TextField label="First name" />
+            <TextField label="Last name" />
+            <Button variant="contained" size="large">
+              Save Profile Information
+            </Button>
+          </Stack>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
