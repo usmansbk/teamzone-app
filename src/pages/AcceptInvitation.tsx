81a7c56181981dbf6747368244d707f5067ec7cb
usmansbk <usmansbk@gmail.com>
2022-12-02 23:32:21 +0100

    join team

--- src/pages/AcceptInvitation.tsx
+++ src/pages/AcceptInvitation.tsx
@@ -1,16 +1,44 @@
-import { Box, Typography } from "@mui/material";
-import { useParams } from "react-router-dom";
+import { Box, CircularProgress, Typography } from "@mui/material";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+import { Navigate, useParams } from "react-router-dom";
+import useJoinTeam from "src/hooks/api/useJoinTeam";
+import routeMap from "src/routeMap";
 
 export default function AcceptInvitation() {
   const { code } = useParams<{ code: string }>();
+  const { onSubmit, loading, data, error } = useJoinTeam();
+
+  useEffect(() => {
+    if (code) {
+      onSubmit(code);
+    }
+  }, [code]);
+
+  if (data) {
+    toast.success("Success");
+    return <Navigate to={routeMap.team.replace(":id", data.id)} replace />;
+  }
+
   return (
     <Box
       display="flex"
       flexGrow={1}
       justifyContent="center"
       alignItems="center"
+      flexDirection="column"
     >
-      <Typography variant="h3">{code}</Typography>
+      {loading && <CircularProgress />}
+      {!!error && (
+        <>
+          <Typography variant="h3" sx={{ textAlign: "center" }}>
+            The invite code is invalid.
+          </Typography>
+          <Typography sx={{ textAlign: "center" }}>
+            Please request another link from your team.
+          </Typography>
+        </>
+      )}
     </Box>
   );
 }
