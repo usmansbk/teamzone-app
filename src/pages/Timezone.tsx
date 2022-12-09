1d6fc8aa056c44083751ddf17dab19e5e0ae35fd
usmansbk <usmansbk@gmail.com>
2022-12-09 11:46:08 +0100

    show time diff

--- src/pages/Timezone.tsx
+++ src/pages/Timezone.tsx
@@ -1,8 +1,14 @@
-import { Box, LinearProgress, Typography } from "@mui/material";
+import { Box, LinearProgress, Stack, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 import BigClock from "src/components/BigClock";
 import useGetTimezoneById from "src/hooks/api/useGetTimezoneById";
+import useMe from "src/hooks/api/useMe";
 import useTime from "src/hooks/useTime";
+import {
+  formatDuration,
+  formatUTCOffset,
+  getTimeDifferenceInMs,
+} from "src/utils/dateTime";
 import formatTimezoneName from "src/utils/formatTimezoneName";
 import { TimezoneData } from "src/__generated__/graphql";
 import PageNotFound from "./404";
@@ -12,28 +18,73 @@ interface TimezoneDetailsProps {
   timezone: string;
 }
 
-function TimezoneDetails({ data, timezone }: TimezoneDetailsProps) {
-  const { countryName } = data;
+function Clock({
+  name,
+  countryName,
+  timezone,
+}: {
+  name: string;
+  countryName: string;
+  timezone: string;
+}) {
   const { dateTime } = useTime();
-
   return (
-    <Box p={3}>
+    <Box>
       <Typography variant="h4" fontWeight={400}>
         Time in{" "}
-        <Typography
-          variant="h4"
-          fontWeight={900}
-          display="inline-block"
-          color="primary"
+        <span
+          style={{
+            fontWeight: 900,
+          }}
         >
-          {formatTimezoneName(timezone!)}, {countryName}
-        </Typography>{" "}
+          {name}, {countryName}
+        </span>{" "}
         now
       </Typography>
       <BigClock
         time={dateTime.tz(timezone).format("HH:mm:ss")}
         date={dateTime.tz(timezone).format("dddd, MMMM D, YYYY")}
       />
+    </Box>
+  );
+}
+
+function TimezoneDetails({ data, timezone }: TimezoneDetailsProps) {
+  const { countryName, alternativeName, abbreviation } = data;
+  const { data: me } = useMe();
+
+  const name = formatTimezoneName(timezone);
+  const [city] = name.split(",");
+  const [myCity] = formatTimezoneName(me?.timezone!).split(",");
+
+  const timeDiff = getTimeDifferenceInMs(me?.timezone!, timezone);
+
+  return (
+    <Box p={3}>
+      <Clock name={name} countryName={countryName} timezone={timezone} />
+      <Stack mt={4} spacing={4}>
+        <Box>
+          <Typography variant="h4" color="primary">
+            Time Zone
+          </Typography>
+          <Typography variant="h6">
+            {alternativeName} ({abbreviation})
+          </Typography>
+          <Typography variant="h6">UTC {formatUTCOffset(timezone)}</Typography>
+          <Typography variant="h6">{timezone}</Typography>
+        </Box>
+        {me?.timezone !== timezone && (
+          <Box>
+            <Typography variant="h4" color="primary">
+              Time difference
+            </Typography>
+            <Typography variant="h6">
+              {myCity} is {formatDuration(timeDiff)}{" "}
+              {timeDiff < 0 ? "behind" : "ahead of"} {city}.
+            </Typography>
+          </Box>
+        )}
+      </Stack>
     </Box>
   );
 }
--- src/utils/dateTime.ts
+++ src/utils/dateTime.ts
@@ -3,11 +3,13 @@ import utc from "dayjs/plugin/utc";
 import tz from "dayjs/plugin/timezone";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import duration from "dayjs/plugin/duration";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 dayjs.extend(utc);
 dayjs.extend(tz);
 dayjs.extend(localizedFormat);
 dayjs.extend(duration);
+dayjs.extend(relativeTime);
 
 export function getLocalTime(timezone: string, format = "HH:mm") {
   return dayjs().tz(timezone).format(format);
@@ -27,6 +29,17 @@ export function getLocalDate(timezone: string, format = "dddd, D MMMM, YYYY") {
 
 export function formatUTCOffset(timezone: string) {
   return dayjs().tz(timezone).format("Z");
+}
+
+export function getTimeDifferenceInMs(currentTz: string, targetTz: string) {
+  const now = dayjs();
+  const current = now.tz(currentTz, true);
+  const target = now.tz(targetTz, true);
+  return target.diff(current);
+}
+
+export function formatDuration(ms: number) {
+  return dayjs.duration(ms, "milliseconds").humanize();
 }
 
 export default dayjs;
