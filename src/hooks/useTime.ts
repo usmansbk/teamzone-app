251d658a3542997dbf72701da2e70f0a7f4d319d
usmansbk <usmansbk@gmail.com>
2022-12-09 19:10:43 +0100

    fix timer

--- src/hooks/useTime.ts
+++ src/hooks/useTime.ts
@@ -1,16 +1,30 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { getCurrentDateTime } from "src/utils/dateTime";
 
 export default function useTime() {
   const [dateTime, setDateTime] = useState(getCurrentDateTime());
 
-  useEffect(() => {
-    const ticker = setTimeout(() => {
-      setDateTime(dateTime.add(1, "second"));
-    }, 1000 - new Date().getMilliseconds());
+  const timer = useCallback(() => {
+    let time = dateTime;
+    let clearId: NodeJS.Timeout;
 
-    return () => clearTimeout(ticker);
-  }, [dateTime]);
+    const tick = () => {
+      if (clearId) {
+        clearTimeout(clearId);
+      }
+      time = time.add(1, "second");
+      setDateTime(time);
+      clearId = setTimeout(tick, 1000 - new Date().getMilliseconds());
+    };
+
+    clearId = setTimeout(tick, 1000 - new Date().getMilliseconds());
+  }, []);
+
+  useEffect(() => {
+    const handle = requestAnimationFrame(timer);
+
+    return () => cancelAnimationFrame(handle);
+  }, []);
 
   return {
     dateTime,
