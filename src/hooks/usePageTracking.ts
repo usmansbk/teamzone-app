c5a4ea631daadb779976872c61bd7d399623f50d
usmansbk <usmansbk@gmail.com>
2022-12-10 20:31:32 +0100

    track pages

--- src/hooks/usePageTracking.ts
+++ src/hooks/usePageTracking.ts
@@ -2,12 +2,13 @@ import { useEffect } from "react";
 import ReactGA from "react-ga";
 import { useLocation } from "react-router-dom";
 
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID, {
+  debug: false,
+});
+
 export default function usePageTracking() {
   const location = useLocation();
   useEffect(() => {
-    ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID, {
-      debug: true,
-    });
     ReactGA.pageview(location.pathname + location.search);
   }, [location]);
 }
