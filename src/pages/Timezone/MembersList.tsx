e67348ae802448e94756e83700655383d3574de7
usmansbk <usmansbk@gmail.com>
2022-12-10 01:10:49 +0100

    nit

--- src/pages/Timezone/MembersList.tsx
+++ src/pages/Timezone/MembersList.tsx
@@ -9,7 +9,7 @@ const MembersList = ({ timezone }: { timezone: string }) => {
     return (
       <Grid container mt={2}>
         <Grid item>
-          <Skeleton variant="circular" width={80} height={80} />
+          <Skeleton variant="circular" width={60} height={60} />
           <Skeleton variant="text" sx={{ fontSize: "2rem" }} />
           <Skeleton variant="text" />
         </Grid>
@@ -30,15 +30,15 @@ const MembersList = ({ timezone }: { timezone: string }) => {
             <Stack
               direction={{ xs: "row", md: "column" }}
               alignItems={{ xs: "center", md: "flex-start" }}
-              spacing={1}
+              spacing={2}
             >
               <Avatar
                 alt={member.fullName}
                 src={member.picture}
-                sx={{ width: 80, height: 80 }}
+                sx={{ width: 60, height: 60 }}
               />
               <Stack>
-                <Typography variant="h6">{member.fullName}</Typography>
+                <Typography variant="h5">{member.fullName}</Typography>
                 <Typography variant="subtitle1">{team.name}</Typography>
               </Stack>
             </Stack>
