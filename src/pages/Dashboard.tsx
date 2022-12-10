28ee234fc82379d0b84c5ced08d19b60126a3a8b
usmansbk <usmansbk@gmail.com>
2022-12-10 08:47:43 +0100

    nit

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -35,7 +35,7 @@ const Dashboard = () => {
   ) as TeamMember[];
 
   return (
-    <Box p={2} pt={0}>
+    <Box p={3} pt={0}>
       <Typography variant="h4" fontWeight={400}>
         Time in{" "}
         <Link
