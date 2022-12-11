e82ff4e91cf2e52e1bb4ff02ab570e72919fe952
usmansbk <usmansbk@gmail.com>
2022-12-11 17:56:03 +0100

    nit

--- src/components/MeetingForm.tsx
+++ src/components/MeetingForm.tsx
@@ -6,9 +6,16 @@ import {
   Box,
   FormControlLabel,
   Switch,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
 } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import { MobileDateTimePicker } from "@mui/x-date-pickers";
+import useMe from "src/hooks/api/useMe";
+import { Team } from "src/__generated__/graphql";
+import uniqBy from "lodash.uniqby";
 
 interface Props {
   title: string;
@@ -18,7 +25,18 @@ interface Props {
 
 const DATE_TIME_FORMAT = "MMM DD, YYYY, HH:mm";
 
+const extractTimezones = (teams: Team[]) =>
+  uniqBy(
+    teams.flatMap((t) => t!.teammates),
+    "member.timezone"
+  ).map((t) => t?.member.timezone);
+
 export default function MeetingForm({ title, onClose, loading }: Props) {
+  const { data } = useMe();
+  const { teams, timezone } = data!;
+
+  const timezones = extractTimezones(teams as Team[]);
+
   return (
     <Stack spacing={1} maxWidth="sm">
       <Stack direction="row" justifyContent="space-between" spacing={1}>
@@ -34,6 +52,25 @@ export default function MeetingForm({ title, onClose, loading }: Props) {
       </Stack>
       <Stack spacing={2}>
         <TextField label="Title" placeholder="Add title" />
+        <FormControl>
+          <InputLabel sx={{ fontWeight: 800 }}>Timezone</InputLabel>
+          <Select
+            label="Timezone"
+            value={timezone}
+            placeholder="Select timezone"
+            inputProps={{
+              sx: {
+                fontWeight: 800,
+              },
+            }}
+          >
+            {timezones.map((tz) => (
+              <MenuItem key={tz} value={tz!}>
+                {tz}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
         <Stack>
           <Stack direction={{ xs: "column", md: "row" }} spacing={2}>
             <Box flexGrow={1}>
@@ -72,6 +109,26 @@ export default function MeetingForm({ title, onClose, loading }: Props) {
             />
           </Box>
         </Stack>
+        <FormControl>
+          <InputLabel sx={{ fontWeight: 800 }}>Teams</InputLabel>
+          <Select
+            label="Teams"
+            multiple
+            value={[]}
+            placeholder="Add teams"
+            inputProps={{
+              sx: {
+                fontWeight: 800,
+              },
+            }}
+          >
+            {teams.map((team) => (
+              <MenuItem key={team!.id} value={team!.id}>
+                {team?.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
         <TextField
           placeholder="Add description"
           multiline
