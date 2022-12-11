fa4d6f00b51bf9aa6cb4fa9c7b94278a7d8a4948
usmansbk <usmansbk@gmail.com>
2022-12-11 07:45:40 +0100

    nit

--- src/pages/Calendar/index.tsx
+++ src/pages/Calendar/index.tsx
@@ -1,5 +1,5 @@
 import { Typography } from "@mui/material";
 
 export default function Calendar() {
-  return <Typography variant="h1">Calendar</Typography>;
+  return <Typography variant="h1">Under development...</Typography>;
 }
