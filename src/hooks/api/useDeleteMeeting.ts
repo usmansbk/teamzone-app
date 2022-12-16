9ffc95743032fe0c15fef41585e8863217950943
usmansbk <usmansbk@gmail.com>
2022-12-16 12:11:38 +0100

    fix delete

--- src/hooks/api/useDeleteMeeting.ts
+++ src/hooks/api/useDeleteMeeting.ts
@@ -2,7 +2,10 @@ import { useMutation } from "@apollo/client";
 import { useCallback } from "react";
 import deleteMeeting from "src/graphql/queries/deleteMeeting";
 import getMeetings from "src/graphql/queries/getMeetings";
-import { DeleteMeetingMutationVariables } from "src/__generated__/graphql";
+import {
+  DeleteMeetingMutationVariables,
+  MeetingSort,
+} from "src/__generated__/graphql";
 
 export default function useDeleteMeeting() {
   const [mutate, { loading, data, error }] = useMutation(deleteMeeting);
@@ -16,6 +19,30 @@ export default function useDeleteMeeting() {
             cache.updateQuery(
               {
                 query: getMeetings,
+                variables: {
+                  sort: MeetingSort.Upcoming,
+                },
+              },
+              (meetingsData) => {
+                if (!meetingsData) {
+                  return meetingsData;
+                }
+                return {
+                  getMeetings: {
+                    ...meetingsData?.getMeetings,
+                    meetings: meetingsData!.getMeetings.meetings.filter(
+                      (meeting) => meeting?.id !== result.data?.deleteMeeting.id
+                    ),
+                  },
+                };
+              }
+            );
+            cache.updateQuery(
+              {
+                query: getMeetings,
+                variables: {
+                  sort: MeetingSort.Past,
+                },
               },
               (meetingsData) => {
                 if (!meetingsData) {
