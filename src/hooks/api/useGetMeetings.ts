c934d8c5c860b47874439f63b91f18b701599ccb
usmansbk <usmansbk@gmail.com>
2022-12-16 11:48:43 +0100

    fix sort

--- src/hooks/api/useGetMeetings.ts
+++ src/hooks/api/useGetMeetings.ts
@@ -2,22 +2,30 @@ import { useQuery } from "@apollo/client";
 import { useMemo } from "react";
 import getMeetings from "src/graphql/queries/getMeetings";
 import dayjs from "src/utils/dateTime";
-import { GetMeetingsQueryVariables } from "src/__generated__/graphql";
+import {
+  GetMeetingsQueryVariables,
+  MeetingSort,
+} from "src/__generated__/graphql";
 
 export default function useGetMeetings(variables: GetMeetingsQueryVariables) {
   const { loading, data, error } = useQuery(getMeetings, {
     variables,
     notifyOnNetworkStatusChange: true,
+    fetchPolicy: "cache-and-network",
   });
 
   const meetings = useMemo(() => {
     if (data?.getMeetings) {
-      return [...data.getMeetings.meetings].sort((a, b) =>
-        dayjs.utc(a?.from).diff(dayjs(b?.from))
-      );
+      return [...data.getMeetings.meetings].sort((a, b) => {
+        const diff = dayjs.utc(a?.from).diff(dayjs(b?.from));
+        if (variables.sort === MeetingSort.Upcoming) {
+          return diff;
+        }
+        return -diff;
+      });
     }
     return [];
-  }, [data?.getMeetings]);
+  }, [data?.getMeetings, variables.sort]);
 
   return {
     loading,
