b35fbe0bd0569779ce6a0c82d83a1629b4b3fc13
usmansbk <usmansbk@gmail.com>
2022-12-26 11:41:52 +0100

    nit

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,6 +1,5 @@
 import { Add, ArrowBack, ArrowForward } from "@mui/icons-material";
 import {
-  Typography,
   Box,
   Stack,
   Button,
@@ -87,68 +86,61 @@ export default function Calendar() {
   return (
     <Stack p={2} spacing={1}>
       <CalendarHeader date={date} />
-      {meetings?.length === 0 && (
-        <Box p={2}>
-          <Typography variant="h3">No meetings</Typography>
-        </Box>
-      )}
-      {!!meetings.length && (
-        <Grid container rowGap={1}>
-          <Grid item xs={12} lg="auto">
-            <Box
-              sx={{
-                display: {
-                  xs: "none",
-                  lg: "block",
+      <Grid container rowGap={1}>
+        <Grid item xs={12} lg="auto">
+          <Box
+            sx={{
+              display: {
+                xs: "none",
+                lg: "block",
+              },
+            }}
+          >
+            <StaticDatePicker
+              reduceAnimations
+              showDaysOutsideCurrentMonth
+              displayStaticWrapperAs="desktop"
+              value={date}
+              onChange={(value) => {
+                if (value) {
+                  navigate(`?day=${value.tz().format(DATE_FORMAT)}`);
+                }
+              }}
+              renderInput={() => <div />}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: {
+                xs: "block",
+                lg: "none",
+              },
+            }}
+          >
+            <MobileDatePicker
+              reduceAnimations
+              showDaysOutsideCurrentMonth
+              value={date}
+              onChange={(value) => {
+                if (value) {
+                  navigate(`?day=${value.tz().format(DATE_FORMAT)}`);
+                }
+              }}
+              InputProps={{
+                sx: {
+                  fontWeight: 800,
                 },
               }}
-            >
-              <StaticDatePicker
-                reduceAnimations
-                showDaysOutsideCurrentMonth
-                displayStaticWrapperAs="desktop"
-                value={date}
-                onChange={(value) => {
-                  if (value) {
-                    navigate(`?day=${value.tz().format(DATE_FORMAT)}`);
-                  }
-                }}
-                renderInput={() => <div />}
-              />
-            </Box>
-            <Box
-              sx={{
-                display: {
-                  xs: "block",
-                  lg: "none",
-                },
-              }}
-            >
-              <MobileDatePicker
-                reduceAnimations
-                showDaysOutsideCurrentMonth
-                value={date}
-                onChange={(value) => {
-                  if (value) {
-                    navigate(`?day=${value.tz().format(DATE_FORMAT)}`);
-                  }
-                }}
-                InputProps={{
-                  sx: {
-                    fontWeight: 800,
-                  },
-                }}
-                renderInput={(params) => <TextField fullWidth {...params} />}
-                inputFormat="ddd MMMM DD, YYYY"
-                showToolbar={false}
-              />
-            </Box>
-          </Grid>
-          <Grid item flex="auto">
-            <Agenda meetings={meetings as Meeting[]} selectedDate={date} />
-          </Grid>
+              renderInput={(params) => <TextField fullWidth {...params} />}
+              inputFormat="ddd MMMM DD, YYYY"
+              showToolbar={false}
+            />
+          </Box>
         </Grid>
-      )}
+        <Grid item flex="auto">
+          <Agenda meetings={meetings as Meeting[]} selectedDate={date} />
+        </Grid>
+      </Grid>
     </Stack>
   );
 }
